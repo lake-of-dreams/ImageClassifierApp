52804b3ef5b703999031020ebe85ec79016d1d94
Ravi Singhal <ravi.r.singhal@oracle.com>
2017-10-25 18:34:47 +0530

    some components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {
+  Ng4FilesService,
+  Ng4FilesConfig,
+  Ng4FilesSelected,
+  Ng4FilesStatus
+} from 'angular4-files-upload';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  public selectedFiles;
+  
+ public filesSelect(selectedFiles: Ng4FilesSelected): void {
+     if (selectedFiles.status !== Ng4FilesStatus.STATUS_SUCCESS) {
+       this.selectedFiles = selectedFiles.status;
+       return;
+       
+       // Hnadle error statuses here
+     }
+  
+     this.selectedFiles = Array.from(selectedFiles.files).map(file => file.name);
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Ng4FilesModule } from 'angular4-files-upload';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng4FilesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
